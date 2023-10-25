115826fe1c7fdc2250da87b779f6b6f0cfe540f1
iwishyoujoy <dasha@skv.name>
2023-10-25 17:34:47 +0300

    feat:Changed header

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -5,46 +5,26 @@ import logo from "@/images/logo.svg";
 import search from "@/images/search.svg";
 import cart from "@/images/cart.svg";
 import account from "@/images/account.svg";
-import arrowDown from "@/images/arrowDown.svg";
+
+// import arrowDown from "@/images/arrowDown.svg";
 
 import Link from "next/link";
-import {
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-  } from '@chakra-ui/react';
-
 
 export default function Header(){
     return (
         <header>
-            <div className={styles.logoContainer}>
-                <Image className={styles.logo} src={logo} alt="Logo picture - heels"/>
-                <div className={styles.logoText}>Bimbo shop</div>
-            </div>
-            <div className={styles.menuContainer}>
-                <Link href="/">Home</Link>
-                <Link href="/">Catalog</Link>
-                <Menu>
-                    <MenuButton rightIcon={<div>{arrowDown}</div>}>
-                        Shop
-                    </MenuButton>
-                    {arrowDown}
-                    <MenuList>
-                        <MenuItem as='a' href='#'>Fashion</MenuItem>
-                        <MenuItem as='a' href='#'>Procedures</MenuItem>
-                    </MenuList>
-                </Menu>
-                <Menu>
-                    <MenuButton rightIcon={<div>{arrowDown}</div>}>
-                        Pages   
-                    </MenuButton>
-                    <MenuList>
-                        <MenuItem as='a' href='#'>Blog</MenuItem>
-                        <MenuItem as='a' href='#'>Something</MenuItem>
-                    </MenuList>
-                </Menu>
+            <div className={styles.leftContainer}>
+                <div className={styles.logoContainer}>
+                    <Image className={styles.logo} src={logo} alt="Logo picture - heels"/>
+                    <div className={styles.logoText}>Bimbo shop</div>
+                </div>
+                <div className={styles.menuContainer}>
+                    <Link className={styles.hoverPink} href="/">Home</Link>
+                    <Link className={styles.hoverPink} href="/">Catalog</Link>
+                    {/* заменить на кастомные дропдауны */}
+                    <Link className={styles.hoverPink} href="/">Shop</Link>
+                    <Link className={styles.hoverPink} href="/">Pages</Link>
+                </div>
             </div>
             <div className={styles.accountContainer}>
                 <Link href="/">
