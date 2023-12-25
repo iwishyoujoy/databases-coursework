65e30fb1d17f606f6b77aea1ac43816a1667fc55
Батомункуева Виктория <torichan373737@gmail.com>
2023-12-25 20:44:20 +0300

    добавила генераторы ид

--- backend/src/main/java/com/example/server/model/Clinic.java
+++ backend/src/main/java/com/example/server/model/Clinic.java
@@ -2,6 +2,8 @@ package com.example.server.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
@@ -15,6 +17,7 @@ import lombok.*;
 public class Clinic {
     @Id
     @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     @Column(name = "name", nullable = false)
     private String name;
--- backend/src/main/java/com/example/server/model/Customer.java
+++ backend/src/main/java/com/example/server/model/Customer.java
@@ -2,6 +2,8 @@ package com.example.server.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
@@ -17,6 +19,7 @@ import java.sql.Date;
 public class Customer {
     @Id
     @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     @Column(name = "name", nullable = false)
     private String name;
