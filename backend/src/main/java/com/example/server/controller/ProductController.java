93a95ce7c8d51c02331c870bcd9f893c2a71f68c
Батомункуева Виктория <torichan373737@gmail.com>
2024-01-06 15:39:35 +0300

    Product: удаление

--- backend/src/main/java/com/example/server/controller/ProductController.java
+++ backend/src/main/java/com/example/server/controller/ProductController.java
@@ -1,12 +1,11 @@
 package com.example.server.controller;
 
 import com.example.server.model.Product;
-import com.example.server.model.Appointment;
 import com.example.server.model.Item;
 import com.example.server.model.ItemInOrder;
-import com.example.server.model.Procedure;
 import com.example.server.repo.ItemRepo;
 import com.example.server.repo.ProductRepo;
+import com.example.server.repo.ItemInOrderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,11 +21,13 @@ import java.util.NoSuchElementException;
 public class ProductController {
     private final ProductRepo productRepo;
     private final ItemRepo itemRepo;
+    private final ItemInOrderRepo itemInOrderRepo;
 
     @Autowired
-    public ProductController(ProductRepo productRepo, ItemRepo itemRepo) throws NoSuchAlgorithmException {
+    public ProductController(ProductRepo productRepo, ItemRepo itemRepo, ItemInOrderRepo itemInOrderRepo) throws NoSuchAlgorithmException {
         this.productRepo = productRepo;
         this.itemRepo = itemRepo;
+        this.itemInOrderRepo = itemInOrderRepo;
     }
 
     @PostMapping("create/")
@@ -67,6 +68,9 @@ public class ProductController {
         try {
             Product product = productRepo.findAll().stream().filter(user -> user.getId_item().equals(Long.parseLong(id))).findFirst().get();
             Item item = itemRepo.findAll().stream().filter(user -> user.getId().equals(Long.parseLong(id))).findFirst().get();
+            List<ItemInOrder> itemInOrders = itemInOrderRepo.findAll().stream().filter(user -> user.getItemInOrderId().getItem_id().equals(Long.parseLong(id))).toList(); 
+            for(ItemInOrder itemInOrder: itemInOrders)
+                itemInOrderRepo.delete(itemInOrder);
             productRepo.delete(product);
             itemRepo.delete(item);
             return ResponseEntity.status(HttpStatus.OK).build();
@@ -74,28 +78,6 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
-
-    // @DeleteMapping("{id}")
-    // public ResponseEntity<Void> delete(@PathVariable Long id) {
-    //     try {
-    //         Procedure procedure = procedureRepo.findAll().stream().filter(user -> user.getId() == id).findFirst().get();
-    //         List<Appointment> appointments = appointmentRepo.findAll().stream().filter(user -> user.getProcedure_id().equals(id)).toList();
-    //         List<Long> appointmentsIds = new ArrayList<>();
-    //         for(Appointment appointment : appointments)
-    //             appointmentsIds.add(appointment.getItem_id());
-    //         List<ItemInOrder> itemInOrders = itemInOrderRepo.findAll().stream().filter(user -> appointmentsIds.contains(user.getItemInOrderId().getItem_id())).toList(); 
-    //         for(ItemInOrder itemInOrder: itemInOrders)
-    //             itemInOrderRepo.delete(itemInOrder);
-    //         for(Appointment appointment : appointments)
-    //             appointmentRepo.delete(appointment);
-            
-    //         procedureRepo.delete(procedure);
-    //         return ResponseEntity.status(HttpStatus.OK).build();
-    //     } catch (NoSuchElementException e) {
-    //         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-    //     }
-    // }
-
 
 
     @PutMapping("{id}")
