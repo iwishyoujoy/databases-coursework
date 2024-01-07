aed01902503e09bde540f0cf68614b09e06f464c
Батомункуева Виктория <torichan373737@gmail.com>
2024-01-07 17:13:30 +0300

    Seller: get продукты по id селлера

--- backend/src/main/java/com/example/server/controller/SellerController.java
+++ backend/src/main/java/com/example/server/controller/SellerController.java
@@ -1,7 +1,7 @@
 package com.example.server.controller;
 
-import com.example.server.model.Seller;
-import com.example.server.repo.SellerRepo;
+import com.example.server.model.*;
+import com.example.server.repo.*;
 import com.example.server.service.AuthRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +11,12 @@ import org.springframework.web.bind.annotation.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.NoSuchElementException;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/seller")
@@ -18,11 +24,23 @@ import java.util.NoSuchElementException;
 public class SellerController {
 
     private final SellerRepo sellerRepo;
+    private final BimboOrderRepo bimboOrderRepo;
+    private final ProductRepo productRepo;
+    private final ItemRepo itemRepo;
+    private final ItemInOrderRepo itemInOrderRepo;
     private final MessageDigest md = MessageDigest.getInstance("SHA-512");
 
     @Autowired
-    public SellerController(SellerRepo sellerRepo) throws NoSuchAlgorithmException {
+    public SellerController(SellerRepo sellerRepo, 
+                            BimboOrderRepo bimboOrderRepo, 
+                            ProductRepo productRepo, 
+                            ItemRepo itemRepo, 
+                            ItemInOrderRepo itemInOrderRepo) throws NoSuchAlgorithmException {
         this.sellerRepo = sellerRepo;
+        this.bimboOrderRepo = bimboOrderRepo;
+        this.productRepo = productRepo;
+        this.itemRepo = itemRepo;
+        this.itemInOrderRepo = itemInOrderRepo;
     }
 
     @PostMapping("signin/")
@@ -70,7 +88,22 @@ public class SellerController {
         } catch(NoSuchElementException e){
             return ResponseEntity.badRequest().body(null);
         }
+    }
 
+
+    @GetMapping("order/{id}")
+    public ResponseEntity<Map<Long, List<ItemInOrder>>> getOrders(@PathVariable Long id) {
+        try{
+            List<Product> products = productRepo.findAll().stream().filter(user -> user.getSeller_id() == id).toList();
+            List<Long> ids = new ArrayList<>();
+            for(Product product : products)
+                ids.add(product.getId_item());
+            List<ItemInOrder> itemInOrders = itemInOrderRepo.findAll().stream().filter(user -> ids.contains(user.getItemInOrderId().getItem_id())).toList();
+            Map<Long, List<ItemInOrder>> groupedOrders = itemInOrders.stream().collect(Collectors.groupingBy(item -> item.getItemInOrderId().getOrder_id()));
+            return ResponseEntity.ok().body(groupedOrders);
+        } catch(NoSuchElementException e){
+            return ResponseEntity.badRequest().body(null);
+        }
     }
 
     @DeleteMapping("{login}")
